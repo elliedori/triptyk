b8912dfa7a165877b8b63b4854b1160851e1ab11
Ellie Dori <email": null>
2016-10-10 16:18:19 -0700

    Generate users, destination, and trip migrations

---
+++ db/migrate/20161010161640_create_destinations_table.rb
@@ -0,0 +1,5 @@
+class CreateDestinationsTable < ActiveRecord::Migration
+  def change
+    create_table
+  end
+end
---
+++ db/migrate/20161010161649_create_trips_table.rb
@@ -0,0 +1,4 @@
+class CreateTripsTable < ActiveRecord::Migration
+  def change
+  end
+end
