21fe604797dfd9bf6c9738b746c63e8d4a21e0d0
elliedori <elliedori@gmail.com>
2016-10-12 22:22:32 -0700

    Add code to extract image url from json object

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,6 @@
 get '/' do
   @photo = Unsplash::Photo.find("tAKXap853rY")
-  p @photo
-  data = JSON.parse(File.read(@photo), symbolize_keys: true)
+  @link = @photo.links["html"]
+  # data = JSON.parse(File.read(@photo), symbolize_keys: true)
   erb :index
 end
