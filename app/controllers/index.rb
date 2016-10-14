de9cac80eb50accdc5bae5f7cf31d9f8fa69e808
Ellie Dori <email": null>
2016-10-14 12:33:54 -0700

    Add map/travel photo to homepage

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,6 @@
 get '/' do
-  @photos_json_obj = Unsplash::Photo.search("Cambodia")
-  @link = @photos_json_obj.first.urls["regular"]
+  # @photos_json_obj = Unsplash::Photo.search("Dariusz Sankowski")
+  # p @photos_json_obj.length
+  # @link = @photos_json_obj.first.urls["regular"]
   erb :index
 end
