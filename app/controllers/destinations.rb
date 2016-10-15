590302289aed8a3c23810f7e4ad04e4242ffc481
Ellie Dori <email": null>
2016-10-14 18:19:26 -0700

    Delete old version of refactored code

--- app/controllers/destinations.rb
+++ app/controllers/destinations.rb
@@ -4,15 +4,9 @@ post '/destinations' do
 end
 
 get '/destinations/:name' do
-  p params
   @destination = params[:name]
   @photos_json_obj = Unsplash::Photo.search(@destination)
   @link = @photos_json_obj.first.urls["regular"]
   @current_users_list = current_user.destinations.pluck(:name)
-  # current_user.destinations.each do |place|
-  #   @current_users_list << place.name
-  # end
-  puts "*********"
-  p @current_users_list
   erb :'/destinations/show'
 end
