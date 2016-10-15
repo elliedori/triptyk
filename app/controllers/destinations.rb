2a69124431ad62bdf95e9f60db76eb1c679d6670
Ellie Dori <email": null>
2016-10-14 18:29:40 -0700

    Sanitize query against spaces and capitalization

--- app/controllers/destinations.rb
+++ app/controllers/destinations.rb
@@ -1,10 +1,14 @@
 post '/destinations' do
+  puts "********"
+  p params
   @destination = params[:destination].downcase
+  @destination.gsub!(" ", "%20")
   redirect "/destinations/#{@destination}"
 end
 
 get '/destinations/:name' do
   @destination = params[:name]
+  @destination.gsub!("%20", " ")
   @photos_json_obj = Unsplash::Photo.search(@destination)
   @link = @photos_json_obj.first.urls["regular"]
   @current_users_list = current_user.destinations.pluck(:name)
