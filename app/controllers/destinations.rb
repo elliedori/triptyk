dabdddd9853475f3973ef6dfc844a71640efe49b
Ellie Dori <email": null>
2016-10-14 18:42:48 -0700

    Add error message if unsplash returns no images

--- app/controllers/destinations.rb
+++ app/controllers/destinations.rb
@@ -10,7 +10,11 @@ get '/destinations/:name' do
   @destination = params[:name]
   @destination.gsub!("%20", " ")
   @photos_json_obj = Unsplash::Photo.search(@destination)
-  @link = @photos_json_obj.first.urls["regular"]
-  @current_users_list = current_user.destinations.pluck(:name)
-  erb :'/destinations/show'
+  if @photos_json_obj.first
+    @link = @photos_json_obj.first.urls["regular"]
+    @current_users_list = current_user.destinations.pluck(:name) if logged_in?
+    erb :'/destinations/show'
+  else
+    "Please enter a less specific location"
+  end
 end
