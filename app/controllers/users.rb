0be7a28934ff0956a3f1eb82fee11b399ff9e039
elliedori <elliedori@gmail.com>
2016-10-15 22:18:48 -0700

    Change redirect route after clicking destination heart to same page to show the heart is now selected/deselected

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,14 +6,14 @@ end
 put '/my_wanderlist/:destination' do
     destination = Destination.find_or_create_by(name: params[:destination])
     new_trip = Trip.find_or_create_by(user_id: session[:id], destination_id: destination.id)
-    redirect '/my_wanderlist'
+    redirect "/destinations/#{params[:destination]}"
 end
 
 delete '/my_wanderlist/:destination' do
   destination_id = Destination.find_by(name: params[:destination])
   unwanted_trip = Trip.where(user_id: session[:id], destination_id: destination_id).first
   unwanted_trip.destroy
-  redirect '/my_wanderlist'
+  redirect "/destinations/#{params[:destination]}"
 end
 
 
