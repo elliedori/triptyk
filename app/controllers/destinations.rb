8e576e706e5db824a0694e44b72162f2f33def39
Ellie Dori <email": null>
2016-10-17 16:29:02 -0700

    Add code to destination route to extract video ID from youtube api response

--- app/controllers/destinations.rb
+++ app/controllers/destinations.rb
@@ -8,11 +8,11 @@ end
 get '/destinations/:name' do
   @destination = params[:name]
   @destination.gsub!("%20", " ")
-  @photos_json_obj = Unsplash::Photo.search(@destination)
-  youtube = YouTubeCustomSearch.custom_search("traditional indian music")
-  p youtube
-  if @photos_json_obj.first
-    @link = @photos_json_obj.first.urls["regular"]
+  photos_json_obj = Unsplash::Photo.search(@destination)
+  youtube_obj = YouTubeCustomSearch.custom_search("traditional #{@destination} music")
+  @video_id = youtube_obj["items"][0]["id"]["videoId"]
+  if photos_json_obj.first
+    @link = photos_json_obj.first.urls["regular"]
     @current_users_list = current_user.destinations.pluck(:name) if logged_in?
     erb :'/destinations/show'
   else
