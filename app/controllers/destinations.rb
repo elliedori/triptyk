f7cfbd8904906cad836af2c0da5ba6725b132e02
Ellie Dori <email": null>
2016-10-17 17:50:54 -0700

    Add parsing code to find searchable location based on user input that returns photos from unsplash

--- app/controllers/destinations.rb
+++ app/controllers/destinations.rb
@@ -1,7 +1,6 @@
 post '/destinations' do
-  puts "**********"
-  p params
   @destination = params[:destination].downcase
+  p @destination
   @destination.gsub!(" ", "%20")
   redirect "/destinations/#{@destination}"
 end
@@ -9,14 +8,22 @@ end
 get '/destinations/:name' do
   @destination = params[:name]
   @destination.gsub!("%20", " ")
-  photos_json_obj = Unsplash::Photo.search(@destination)
+
   youtube_obj = YouTubeCustomSearch.custom_search("traditional #{@destination} music")
   @video_id = youtube_obj["items"][0]["id"]["videoId"]
-  if photos_json_obj.first
-    @link = photos_json_obj.first.urls["regular"]
+
+  @details = @destination.split(", ")
+  @found_place = @details.find{|query| Unsplash::Photo.search(query).first }
+
+  if @found_place == nil
+    @error = "Please enter a more general location"
+    erb :index
+  else
+  @photos_json_obj = Unsplash::Photo.search(@found_place)
+  @photos_json_obj.first
+    @link = @photos_json_obj.first.urls["regular"]
     @current_users_list = current_user.destinations.pluck(:name) if logged_in?
     erb :'/destinations/show'
-  else
-    "Please enter a more general location"
   end
+
 end
